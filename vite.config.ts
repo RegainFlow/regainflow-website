002064372f0c1a9c63b00e90839ab7b6cce40f78
R3d5tatic <lramirez.army@gmail.com>
2025-11-19 01:10:19 -0500

    fix(deployment): configured netlify deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 import { reactRouter } from '@react-router/dev/vite';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
-
+import netlifyReactRouter from '@netlify/vite-plugin-react-router';
 export default defineConfig({
-  plugins: [reactRouter(), tsconfigPaths()],
-  optimizeDeps: {},
-  ssr: {}
+  plugins: [reactRouter(), tsconfigPaths(), netlifyReactRouter()]
 });
